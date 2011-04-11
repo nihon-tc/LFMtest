3de1c351ea48aee03832575aced51b27b956b7fc
kimukou <kimukou.buzz@gmail.com>
2011-04-11 12:05:06 +0900

    UTF-8 apply setting

--- win32JNA/jnaer.bat
+++ win32JNA/jnaer.bat
@@ -1,5 +1,6 @@
 set GRADLE_HOME=D:\Tooldev\gradle-1.0-milestone-2
 set JAVA_HOME=c:\opt\jdk
+set JAVA_OPTS=-Dgroovy.source.encoding=UTF-8 -Dfile.encoding=UTF-8
 
 set PATH=%GRADLE_HOME%/bin;%JAVA_HOME%/bin
 
