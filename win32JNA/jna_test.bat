9ce3b15fd751c968e9e877957ae75d09abf1d9aa
kimukou <kimukou.buzz@gmail.com>
2011-04-27 18:32:08 +0900

    test

--- win32JNA/jna_test.bat
+++ win32JNA/jna_test.bat
@@ -1,15 +1,18 @@
 set GRADLE_HOME=D:\Tooldev\gradle-1.0-milestone-2
 set JAVA_HOME=c:\opt\jdk
 set JAVA_OPTS=-Dgroovy.source.encoding=UTF-8 -Dfile.encoding=UTF-8
+::set CMD_HOME=C:\WINDOWS\system32
 
-set PATH=%GRADLE_HOME%/bin;%JAVA_HOME%/bin
+set PATH=%GRADLE_HOME%/bin;%JAVA_HOME%/bin;%CMD_HOME%
 
 ::set VISUAL_STUDIO_HOME=../header/Microsoft Visual Studio 10.0
 ::set WINDOWS_SDK_HOME=../header/Microsoft SDKs/Windows/v7.0A
 
-call gradle jnaerator_cnv
-::call gradle jnaerator_clean jnaerator_cnv
-::call gradle jnaerator_cnv > log_lib 2>&1
-::gradle %*
 
-pause
+::call gradle clean jar
+call gradle test %*
+::call gradle jar > log 2>&1
+::call gradle %*
+::chcp 65001
+
+::pause
