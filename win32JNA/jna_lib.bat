838863b5b64357c325d8a9895e816763f5214c14
kimukou <kimukou.buzz@gmail.com>
2011-04-28 13:50:16 +0900

    update gradle 1.0-m3

--- win32JNA/jna_lib.bat
+++ win32JNA/jna_lib.bat
@@ -1,4 +1,4 @@
-set GRADLE_HOME=D:\Tooldev\gradle-1.0-milestone-2
+set GRADLE_HOME=D:\Tooldev\gradle-1.0-milestone-3
 set JAVA_HOME=c:\opt\jdk
 set JAVA_OPTS=-Dgroovy.source.encoding=UTF-8 -Dfile.encoding=UTF-8
 
